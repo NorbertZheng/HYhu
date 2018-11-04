6d68121cb9428ab2a4ceb87c5dd00b70967e715f
fassial <zhenghui19991217@163.com>
2018-11-05 01:59:19 +0800

    修改了settings里面的templates项

--- HYhu/settings.py
+++ HYhu/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'HYhu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
